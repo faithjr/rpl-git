ad4ae7201a4f6509d4087546016432d044efc604
GERRY FERNANDO LESMANA <i16050@ftis>
2018-11-02 14:10:02 +0700

    init

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public Calculator{
+
+public float plus(float a, float b){
+
+}
+
+public float minus(float a, float b){
+
+}
+
+public float multiply(float a, float b){
+
+}
+
+public float divide(float a, float b){
+
+}
+
+
+}
